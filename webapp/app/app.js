1e0085da795d92d8c959c03e0ce35f9f5b13b5d9
mehdad <m.mohamadi313@gmail.com>
2016-02-02 22:36:22 +0330

    extra routes removed

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -24,14 +24,6 @@ angular.module('RS_webApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
       });
   });
 
