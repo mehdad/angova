79515dba2014d70bee3acde729d8a3999daf843a
mehdad <m.mohamadi313@gmail.com>
2016-02-18 15:57:11 +0330

    Create add paged added without functionallity

--- webapp/app/modules/ads/config/ads.client.routes.js
+++ webapp/app/modules/ads/config/ads.client.routes.js
@@ -1,7 +1,10 @@
 "use strict";
 angular.module('ads').config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        .when('/',{
-            templateUrl: 'modules/ads/views/list-ads.client.view.html'
-        });
+    .when('/',{
+        templateUrl: 'modules/ads/views/list-ads.client.view.html'
+    })
+    .when('/ads/create',{
+        templateUrl: 'modules/ads/views/create-ads.client.view.html'
+    });
 }]);
