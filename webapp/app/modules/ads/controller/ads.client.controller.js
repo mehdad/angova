06d7d4c9ab0157c53e38b547a47f45ec7d2f6e72
mehdad <m.mohamadi313@gmail.com>
2016-02-21 08:15:40 +0330

    create Ad page's functionality added

--- webapp/app/modules/ads/controller/ads.client.controller.js
+++ webapp/app/modules/ads/controller/ads.client.controller.js
@@ -1,4 +1,4 @@
-angular.module('ads').controller('AdsController', ['$scope','Ads','AdsConfig','Authentication',function ($scope,Ads,AdsConfig,Authentication) {
+angular.module('ads').controller('AdsController', ['$scope','Ads','AdsConfig','Authentication','$location',function ($scope, Ads, AdsConfig, Authentication, $location) {
 "use strict";
     $scope.authentication = Authentication;
     $scope.find = function() {
@@ -6,6 +6,25 @@ angular.module('ads').controller('AdsController', ['$scope','Ads','AdsConfig','A
     };
 
     $scope.create = function () {
-        var ad = new Ads({});
+
+        var ad = new Ads({
+            user: Authentication.user,
+            favorate: '0',
+            title: this.data.title,
+            description: this.data.description,
+            type: this.data.type==null ? 0 : parseInt(this.data.type),
+            price: this.data.price==null ? 0 : parseInt(this.data.price) ,
+            lease: this.data.lease==null ? 0 : parseInt(this.data.lease),
+            mortgage: this.data.mortgage==null ? 0 : parseInt(this.data.mortgage)
+        });
+        console.log(ad);
+        ad.$save(function(response){
+            console.log('yes');
+            console.log(response);
+            $location.path('ads/'+response._id);
+        },function(error){
+            console.log('no');
+            console.log(error);
+        });
     }
 }]);
