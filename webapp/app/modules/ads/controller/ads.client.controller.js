bd6176b930472f6dec2f8edfa5e18e35ea5d8add
mehdad <m.mohamadi313@gmail.com>
2016-02-21 08:43:45 +0330

    dynamic ad details added

--- webapp/app/modules/ads/controller/ads.client.controller.js
+++ webapp/app/modules/ads/controller/ads.client.controller.js
@@ -1,9 +1,18 @@
-angular.module('ads').controller('AdsController', ['$scope','Ads','AdsConfig','Authentication','$location',function ($scope, Ads, AdsConfig, Authentication, $location) {
+angular.module('ads').controller('AdsController', ['$scope','Ads','AdsConfig','Authentication','$location','$routeParams',function ($scope, Ads, AdsConfig, Authentication, $location, $routeParams) {
 "use strict";
     $scope.authentication = Authentication;
     $scope.find = function() {
         $scope.ads = Ads.query({limit:AdsConfig.adsPerPage});
     };
+
+    $scope.findOne = function(id){
+        var query = Ads.get({AdId: $routeParams.id}).$promise.then(function (data) {
+            $scope.ad = data;
+            console.log($scope.ad);
+            $scope.test="mehdad";
+        });
+
+    }
 
     $scope.create = function () {
 
@@ -15,9 +24,9 @@ angular.module('ads').controller('AdsController', ['$scope','Ads','AdsConfig','A
             type: this.data.type==null ? 0 : parseInt(this.data.type),
             price: this.data.price==null ? 0 : parseInt(this.data.price) ,
             lease: this.data.lease==null ? 0 : parseInt(this.data.lease),
-            mortgage: this.data.mortgage==null ? 0 : parseInt(this.data.mortgage)
+            mortgage: this.data.mortgage==null ? 0 : parseInt(this.data.mortgage),
+            address: this.data.address
         });
-        console.log(ad);
         ad.$save(function(response){
             console.log('yes');
             console.log(response);
--- webapp/app/modules/ads/services/ads.client.service.js
+++ webapp/app/modules/ads/services/ads.client.service.js
@@ -1,6 +1,6 @@
 "use strict";
 angular.module('ads').factory('Ads', ['$resource', function ($resource){
-    return $resource('http://localhost:1337/ad:AdId', {
+    return $resource('http://localhost:1337/ad/:AdId', {
         AdId: '@_id'
     },{
         update: {
